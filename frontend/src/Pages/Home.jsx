2046cf0c64dd7d097a70789e4cfac37ec5acb5c0
Yasin-shaik <skyasin2004@gmail.com>
2025-03-18 15:22:07 +0530

    Backend initiated

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import Carousel from '../Components/Carousel';
 export default function Home() {
   return (
-  <div>
-    <div>
+  <>
+    <div class="container">
       <h1>Top News:</h1>
       <Carousel/>
     </div>
-    
-  </div>
+  </>
   );
 }
